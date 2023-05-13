4f110a617327a74fb63fb70a92666c1111f70695
buthed010203 <naguiar010203@gmail.com>
2023-05-13 05:24:55 -0400

    - Fix UnAssistPath - Fix tile rotate logs - Fix vanilla crash (great job team)

--- arc-core/src/arc/util/ArcRuntimeException.java
+++ arc-core/src/arc/util/ArcRuntimeException.java
@@ -1,8 +1,6 @@
 package arc.util;
 
 public class ArcRuntimeException extends RuntimeException{
-    private static final long serialVersionUID = 6735854402467673117L;
-
     public ArcRuntimeException(String message){
         super(message);
     }
