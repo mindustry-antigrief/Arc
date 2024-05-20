8d8c57e7124a5ffff4b264e0415c094c9914d4cd
buthed010203 <naguiar010203@gmail.com>
2024-05-20 11:21:16 -0400

    Fix music loading crashing the game when the cache directory contains non-standard characters

--- arc-core/src/arc/audio/DownloadableAudio.java
+++ arc-core/src/arc/audio/DownloadableAudio.java
@@ -1,14 +1,12 @@
 package arc.audio;
 
 import arc.files.*;
-import arc.util.*;
 
 public interface DownloadableAudio {
 
     void load(Fi file);
 
-    default ArcRuntimeException loadDirectly(Fi dest){
-        load(dest);
-        return null;
+    default void load(Fi file, boolean alreadyRenamed){
+        load(file);
     }
 }
